1b9e8f85435b1f3aaf38b979361915d455a4fb58
Korirmitchelle <korirmitchelle@gmail.com>
2016-07-25 11:36:28 +0300

    initialize

--- js/app.js
+++ js/app.js
@@ -198,3 +198,20 @@ $(document).foundation()
             });
         };
     }(jQuery));
+
+
+var o = {
+    init: function () {
+        this.portfolio.init();
+    },
+    portfolio: {
+        data: {},
+        init: function () {
+            $('#portfolio').portfolio(o.portfolio.data);
+        }
+    }
+}
+
+$(function () {
+    o.init();
+});
